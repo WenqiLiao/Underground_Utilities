f54538db7d6f055be3f82515c2df841dcc939782
Jasper H <127416190+YoungTaco10@users.noreply.github.com>
2023-08-01 13:51:57 -0400

    Update block_blocks.py

--- block_blocks.py
+++ block_blocks.py
@@ -3,8 +3,8 @@ def edge_in_block(hydrant1, hydrant2):
 
 def blocked_by_block(hydrant1, hydrant2):
     if edge_in_block(hydrant1, hydrant2):
-        for roadID in hydrant1.road_ID:
-           if roadID in hydrant2.road_ID:
+        for road_id in hydrant1.roadID:
+           if road_id in hydrant2.roadID:
                 return False  
         return True
     else:
