0d9142d0b4f9500b045c008572e8059e2cc674b2
Jasper H <127416190+YoungTaco10@users.noreply.github.com>
2023-08-04 15:54:47 -0400

    Update and rename cost_slope.ipynb to methods/cost_slope.py

--- methods/cost_slope.py
+++ methods/cost_slope.py
@@ -1,4 +1,4 @@
-def costSlope(hydrant_i, hydrant_j)
+def costSlope(hydrant_i, hydrant_j):
   rise = hydrant_j.elevationID - hydrant_i.elevationID
   run = hydrant_j.distance(hydrant_i)
   slope = rise/run
