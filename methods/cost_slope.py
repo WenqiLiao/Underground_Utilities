3fe16017f56f817698e6d48e4420e5be761b021f
Jasper H <127416190+YoungTaco10@users.noreply.github.com>
2023-08-08 13:53:57 -0400

    Update cost_slope.py

--- methods/cost_slope.py
+++ methods/cost_slope.py
@@ -1,7 +1,53 @@
-def costSlope(hydrant_i, hydrant_j):
-  rise = hydrant_j.elevationID - hydrant_i.elevationID
-  run = hydrant_j.distance(hydrant_i)
-  slope = rise/run
-  if slope < 0:
-    return 1
-  return slope / x 
+# sorry I know this is repetitive but I cannot find any other way at this point
+def find_max_min_slope(edges):
+    max_slope = float('-inf')
+    min_slope = float('inf')
+
+    for edge in edges:
+        point_i = points[edge.p1]
+        point_j = points[edge.p2]
+        hydrant_i = hydrant_instances[edge.p1]
+        hydrant_j = hydrant_instances[edge.p2]
+        rise = hydrant_j.elevation - hydrant_i.elevation
+        run = distance(point_i, point_j)
+        slope = rise / run
+
+        # Check for maximum slope
+        if slope > max_slope:
+            max_slope = slope
+
+        # Check for minimum slope
+        if slope < min_slope:
+            min_slope = slope
+
+    return max_slope, min_slope
+
+
+max_length = edge_df['length'].max()
+min_length = edge_df['length'].min()
+max_angle = edge_df['intersection_angle'].max()
+min_angle = edge_df['intersection_angle'].min()
+max_slope, min_slope = find_max_min_slope(edge_set_pro)
+
+
+def normalize_min_max(value, min_val, max_val):
+    return (value - min_val) / (max_val - min_val)
+
+
+def costSlope(edge):
+    hydrant_j = df_point.iloc[edge.p2]
+    hydrant_i = df_point.iloc[edge.p1]
+    rise = hydrant_j.elevation - hydrant_i.elevation
+    run = edge.length
+    slope = rise/run
+    if slope < 0:
+      return 1
+    elif slope small:
+      pass
+    elif slope good:
+      pass
+    elif slope max:
+      pass
+    else:
+      pass
+    return normalize_min_max(slope, 0, max_slope)
